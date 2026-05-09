c0e873d9ad640df2d8f1f4f6829efdfa366f26a9
Mozzie <mozzie009@gmail.com>
2026-05-08 20:52:43 -0600

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,27 @@
+let bigBox;
+let text_posX
+let text_posY
+const text_padding = 10
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+
+  bigBox = createElement("textarea");
+  bigBox.size(width, 100);
+  bigBox.position(0,0)
+  
+  text_posX = text_padding
+  text_posY = bigBox.height + text_padding
+//  console. log(bigBox. width)
+}
+
+
+function draw() {
+  background(220);
+  textSize(32);
+  
+  
+textLeading(20);   // ← this was the missing piece
+text(bigBox.value(), text_posX, text_posY, width - (text_padding*2), 400);
+
+}
